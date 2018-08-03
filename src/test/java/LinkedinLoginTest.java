91e363faaf5bd8af392b4629e2741288b777a2de
MPodbelsky <147852369gggG>
2018-08-03 23:18:33 +0300

    Lesson_3_HomeWork 03.08.2018

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -6,11 +6,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LinkedinLoginTest {
-    //@Test //annotation
-    //public void successfulLoginTest() {
-
-
-    // }
+    @Test //annotation
+    public void successfulLoginTest() throws InterruptedException {
+        WebDriver browser = new FirefoxDriver();
+        browser.get("https://www.linkedin.com/");
+        WebElement userEmailField = browser.findElement(By.xpath("//input[@class='login-email']")) ;
+        WebElement userPasswordField = browser.findElement(By.xpath("//input[@class='login-password']")) ;
+        WebElement singInButton = browser.findElement(By.xpath("//input[@class='login submit-button']"));
+        userEmailField.sendKeys("mathewsw1648@gmail.com");
+        userPasswordField.sendKeys("G147852369");
+        singInButton.click();
+        Thread.sleep(3000);
+        Assert.assertEquals(browser.getCurrentUrl(),"https://www.linkedin.com/feed/","Incorrect URL!");
+        Assert.assertEquals(browser.getTitle(),"LinkedIn","Incorrect Title");
+        WebElement launchpadTitle = browser.findElement(By.xpath("//*[@class = 'launchpad__title launchpad__title--is-open Sans-21px-black-85%-dense pb5 fl']"));
+        Assert.assertEquals(launchpadTitle.getText(),"Ilya, начните использовать LinkedIn, выполнив 3 простых шага:","Launchpad title has incorrect message!");
+        browser.close();
+    }
     @Test //annotation
     public void negativeLoginTest() throws InterruptedException {
         WebDriver browser = new FirefoxDriver();
