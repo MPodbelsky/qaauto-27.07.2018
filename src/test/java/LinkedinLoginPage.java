e8fab4dfebea90d9db8852948559395327b81b54
MPodbelsky <147852369gggG>
2018-08-10 15:37:07 +0300

    Lesson_4

---
+++ src/test/java/LinkedinLoginPage.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LinkedinLoginPage {
+    WebDriver browser;
+    WebElement userEmailField;
+    WebElement userPasswordField;
+    WebElement singInButton;
+
+    public LinkedinLoginPage(WebDriver browser) {
+        this.browser = browser;
+        initElements();
+    }
+    private void initElements(){
+        userEmailField = browser.findElement(By.xpath("//input[@id='login-email']"));
+        userPasswordField = browser.findElement(By.xpath("//input[@id='login-password']"));
+        singInButton = browser.findElement(By.xpath("//input[@id='login-submit']"));
+    }
+    public void logIn(String userEmail, String userPass){
+        userEmailField.sendKeys(userEmail);
+        userPasswordField.sendKeys(userPass);
+        singInButton.click();
+
+    }
+
+}
--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -3,40 +3,40 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LinkedinLoginTest {
+    WebDriver browser;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        browser = new FirefoxDriver();
+        browser.get("https://www.linkedin.com/");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        browser.close();
+    }
+
     @Test //annotation
     public void successfulLoginTest() throws InterruptedException {
-        WebDriver browser = new FirefoxDriver();
-        browser.get("https://www.linkedin.com/");
-        WebElement userEmailField = browser.findElement(By.xpath("//input[@class='login-email']")) ;
-        WebElement userPasswordField = browser.findElement(By.xpath("//input[@class='login-password']")) ;
-        WebElement singInButton = browser.findElement(By.xpath("//input[@class='login submit-button']"));
-        userEmailField.sendKeys("mathewsw1648@gmail.com");
-        userPasswordField.sendKeys("G147852369");
-        singInButton.click();
+        LinkedinLoginPage linkedinLoginPage = new  LinkedinLoginPage(browser);
+        linkedinLoginPage.logIn("mathewsw1648@gmail.com","G147852369");
         Thread.sleep(3000);
-        Assert.assertEquals(browser.getCurrentUrl(),"https://www.linkedin.com/feed/","Incorrect URL!");
-        Assert.assertEquals(browser.getTitle(),"LinkedIn","Incorrect Title");
-        WebElement launchpadTitle = browser.findElement(By.xpath("//*[@class = 'launchpad__title launchpad__title--is-open Sans-21px-black-85%-dense pb5 fl']"));
-        Assert.assertEquals(launchpadTitle.getText(),"Ilya, начните использовать LinkedIn, выполнив 3 простых шага:","Launchpad title has incorrect message!");
-        browser.close();
+        Assert.assertEquals(browser.getCurrentUrl(), "https://www.linkedin.com/feed/", "Incorrect URL!");
+        Assert.assertEquals(browser.getTitle(), "LinkedIn", "Incorrect Title");
+        WebElement profileDropdown = browser.findElement(By.xpath("//*[@id='profile-nav-item']"));
+        Assert.assertTrue(profileDropdown.isDisplayed(), "No profile dropdown on home page");
     }
     @Test //annotation
     public void negativeLoginTest() throws InterruptedException {
-        WebDriver browser = new FirefoxDriver();
-        browser.get("https://www.linkedin.com/");
-        WebElement userEmailField = browser.findElement(By.xpath("//input[@class='login-email']")) ;
-        WebElement userPasswordField = browser.findElement(By.xpath("//input[@class='login-password']")) ;
-        WebElement singInButton = browser.findElement(By.xpath("//input[@class='login submit-button']"));
-        userEmailField.sendKeys("Test@gmail.com");
-        userPasswordField.sendKeys("456789123");
-        singInButton.click();
+        LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(browser);
+        linkedinLoginPage.logIn("Test@gmail.com","gfdhhrh");
         Thread.sleep(3000);
         WebElement alertBox = browser.findElement(By.xpath("//*[@role='alert']"));
         Assert.assertEquals(alertBox.getText(),"При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.","Alert box has incorrect message!");
-        browser.close();
     }
 
 }
