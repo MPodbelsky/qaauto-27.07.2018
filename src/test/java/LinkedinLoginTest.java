be708c8056992ff6d0ee124945b0deb058f8c741
MPodbelsky <147852369gggG>
2018-08-17 18:07:13 +0300

    Lesson_6 HomeTask

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -39,18 +39,6 @@ public class LinkedinLoginTest {
         LinkedinLoginSubmitPage linkedinLoginSubmitPage = new LinkedinLoginSubmitPage(browser);
         Assert.assertEquals(linkedinLoginSubmitPage.getAlertBoxText(),"При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.","Alert box has incorrect message!");
     }
-    @Test //annotation
-    public void wrongSymbolsLogin() {
-        linkedinLoginPage.logIn("<mathewsw1648@gmail.com>@gmail.com","G147852369");
-        LinkedinLoginSubmitPage linkedinLoginSubmitPage = new LinkedinLoginSubmitPage(browser);
-        Assert.assertEquals(linkedinLoginSubmitPage.getUserEmailValidationText(),"Укажите действительный адрес эл. почты.","Login Error has incorrect message!");
-    }
-    @Test //annotation
-    public void lowerCasePassword() {
-        linkedinLoginPage.logIn("mathewsw1648@gmail.com","g147852369");
-        LinkedinLoginSubmitPage linkedinLoginSubmitPage = new LinkedinLoginSubmitPage(browser);
-        Assert.assertEquals(linkedinLoginSubmitPage.getUserPasswordValidationText()," Это неверный пароль. Повторите попытку или ","Password has incorrect message!");
-    }
     @DataProvider
     public Object[][] emptyFieldsCombination() {
         return new Object[][]{
@@ -64,13 +52,27 @@ public class LinkedinLoginTest {
         linkedinLoginPage.logIn(userEmail, userPass);
         Assert.assertTrue(linkedinLoginPage.isPageLoaded(),"User is not on Login page");
     }
-    @Test
-    public void validateShortUserEmailAndPassword (){
-        linkedinLoginPage.logIn("a","a");
+    @DataProvider
+    public Object[][] shortUserEmailAndPasswordCombination() {
+        return new Object[][]{
+                { "a", "a", "Слишком короткий текст (минимальная длина – 3 симв., введено – 1 симв.).", "Пароль должен содержать не менее 6 символов."},
+                { "aa", "G147852369", "Слишком короткий текст (минимальная длина – 3 симв., введено – 2 симв.).", ""},
+                {"<mathewsw1648@gmail.com>@gmail.com","G147852369","Укажите действительный адрес эл. почты.",""},
+                {"mathewsw1648@gmail.com","g147852369","","Это неверный пароль. Повторите попытку или измените пароль."},
+                {"mathewsw1648@gmail.co","G147852369","Этот адрес эл. почты не зарегистрирован в LinkedIn. Повторите попытку.",""},
+                {"zxczvvvdvfvdvdvfdvdfvdvdfvvregwer4hryhjhg4wgrgegegergegergefvsrtbtyrukilo;likujhgfedfguioiuhygfd147852369vtjukimusdcdcvsd@gmai.com",
+                        "G147852369",
+                        "Слишком длинный текст: максимальная длина – 128 симв., введено 130 симв.",
+                        ""}
+        };
+    }
+    @Test(dataProvider = "shortUserEmailAndPasswordCombination")
+    public void validateShortUserEmailAndPassword (String userEmail, String userPass, String userEmailValidationText, String userPasswordValidationText){
+        linkedinLoginPage.logIn(userEmail, userPass);
         LinkedinLoginSubmitPage linkedinLoginSubmitPage = new LinkedinLoginSubmitPage(browser);
         Assert.assertTrue(linkedinLoginSubmitPage.isLoaded(), "User is not on LoginSubmit page");
         Assert.assertEquals(linkedinLoginSubmitPage.getAlertBoxText(),"При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.","Alert box has incorrect message!");
-        Assert.assertEquals(linkedinLoginSubmitPage.getUserEmailValidationText(),"Слишком короткий текст (минимальная длина – 3 симв., введено – 1 симв.).","Login error field wrong");
-        Assert.assertEquals(linkedinLoginSubmitPage.getUserPasswordValidationText(),"Пароль должен содержать не менее 6 символов.","Password error field is wrong");
+        Assert.assertEquals(linkedinLoginSubmitPage.getUserEmailValidationText(),userEmailValidationText,"Login error field wrong");
+        Assert.assertEquals(linkedinLoginSubmitPage.getUserPasswordValidationText(),userPasswordValidationText,"Password error field is wrong");
     }
 }
